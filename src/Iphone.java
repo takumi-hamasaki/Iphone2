711f3296f21ee0311b3800bd2dc296db11e06831
takumi-hamasaki <hamasakitakumi0830gmail.com>
2024-10-15 22:08:00 +0900

    20241015

---
+++ src/Iphone.java
@@ -0,0 +1,18 @@
+
+public class Iphone {
+	
+	public static void main(String[] args) {
+		SmartPhone iphone = new SmartPhone();
+		iphone.play();
+		iphone.stop();
+		iphone.next();
+		iphone.back();
+		
+		SmartPhone2 iphone2 = new SmartPhone2();
+		iphone2.call();
+		iphone2.mail();
+		iphone2.photo();
+		iphone2.internet();
+	}
+
+}
